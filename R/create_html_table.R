90083f3099a824894e36f621f26ba4a11663a09c
Han Oostdijk <han@oostdijk-ong.nl>
2026-01-27 19:00:54 +0100

    all pending changes

--- R/create_html_table.R
+++ R/create_html_table.R
@@ -1,14 +1,21 @@
 #' create the html to display a data.frame
 #'
+#' This function is totally depended on the package `htmltools`
+#'
 #' @name create_html_table
 #' @param df1 data.frame for which an html table representation will be made
 #' @param header list of character vectors to use as th table headers. Each vector generates one header row.
-#' @param class Character string with the class name to assign to the table tag
-#' @param caption Character string with the caption for the table
-#' @param tr_class Character string with the class name to assign to the tr tags
+#' @param tableclass Character string with the class name to assign to the table tag
+#' @param tablestyle Character string with the style information to assign to the table tag
+#' @param caption Character string with the table caption
+#' @param captionclass Character string with the class for the table caption
+#' @param captionstyle Character string with the style for the table caption
+#' @param thclass Character string with the class name to assign to the th tags
+#' @param thstyle Character string with the style information to assign to the th tags
+#' @param tdclass Character string with the class name to assign to the td tags
+#' @param tdstyle Character string with the style information to assign to the td tags
 #' @param html_include Character string with html code that is included before the table statement
-#' @param tstyle Character string with the style information to assign to the table tag
-#' @return A list() with a shiny.tag class that can be converted into an HTML string via as.character() and saved to a file with save_html().
+#' @return A list() with a shiny.tag class that can be converted into an HTML string via as.character() and saved to a file with htmltools::save_html().
 #' @export
 #' @examples
 #' \dontrun{
@@ -33,33 +40,39 @@
 #'   "
 #'
 #' df1 <- data.frame(f1=c(1,2),f2=c("A","B"))
-#' st <- create_html_table(df1,tstyle='border-collapse:collapse;border:none;')
-#' st <- create_html_table(df1,class="fit-table",html_include=fit_table_def)
-#' st <- create_html_table(df1,caption="this is my caption")
+#' st <- create_html_table(df1,tablestyle='border-collapse:collapse;border:none;')
+#' st <- create_html_table(df1,tableclass="fit-table",html_include=fit_table_def)
+#' st <- create_html_table(df1,caption="this is my caption",
+#'            captionstyle="white-space: nowrap;caption-side: bottom;")
+#' cat( as.character(st) )
+#' htmltools::save_html(st,'test.html')
 #' }
 #'
 
 
 create_html_table <- function(df1, header = list(names(df1)),
-                              class=NULL,tr_class=NULL, caption=NULL,
-                              html_include = NULL,
-                              tstyle=NULL) {
+                              tableclass=NULL, tablestyle=NULL,
+                              thclass=NULL, thstyle=NULL,
+                              tdclass=NULL, tdstyle=NULL,
+                              caption=NULL, captionclass= NULL, captionstyle=NULL,
+                              html_include = NULL)
+  {
 
-  td <- function(x) {
-  htmltools::tags$td(htmltools::HTML(x), .noWS = "outside")
+  td <- function(x,td_class,td_style) {
+    htmltools::tags$td(htmltools::HTML(x), class=td_class, style=td_style, .noWS = "outside")
   }
 
-  th <- function(x) {
-    htmltools::tags$th(htmltools::HTML(x), .noWS = "outside")
+  th <- function(x,th_class,th_style) {
+    htmltools::tags$th(htmltools::HTML(x), class=th_class, style=th_style, .noWS = "outside")
   }
 
-  tr <- function(x, type = "td",tr_classi=tr_class) {
-    if (type == "th") {
-      htmltools::tags$tr(purrr::map(x, th),class=tr_classi)
-    } else {
-      htmltools::tags$tr(purrr::map(x, td),class=tr_classi)
-    }
-  }
+  # tr <- function(x, type = "td",tr_classi=tr_class) {
+  #   if (type == "th") {
+  #     htmltools::tags$tr(purrr::map(x, th),class=tr_classi)
+  #   } else {
+  #     htmltools::tags$tr(purrr::map(x, td),class=tr_classi)
+  #   }
+  # }
 
   d <- df1 %>%          # table rows
     dplyr::mutate(dplyr::across(where(is.numeric), as.character)) |>
@@ -68,10 +81,12 @@ create_html_table <- function(df1, header = list(names(df1)),
     dplyr::pull(line)          # convert to list of rows
 
   html1 <- htmltools::div(
-      htmltools::tags$table(class = class, style = tstyle,
-        if (!is.null(caption)) htmltools::tags$caption(caption)  ,
-      if (any(0 < purrr::map_dbl(header, length))) purrr::map(header,  ~ tr(., type = "th")),
-      purrr::map(d, tr)
+      htmltools::tags$table(class = tableclass, style = tablestyle,
+        if (!is.null(caption)) htmltools::tags$caption(caption,class=captionclass,style=captionstyle)  ,
+     # if (any(0 < purrr::map_dbl(header, length))) purrr::map(header,  ~ tr(., type = "th")),
+      if (any(0 < purrr::map_dbl(header, length)))
+        purrr::map(header, ~ htmltools::tags$tr(purrr::map(., ~th(.,th_class=thclass,th_style=thstyle)))),
+      purrr::map(d,      ~ htmltools::tags$tr(purrr::map(., ~td(.,td_class=tdclass,td_style=tdstyle))))
       )
    )
 
--- tests/testthat/test-create_html_table.R
+++ tests/testthat/test-create_html_table.R
@@ -51,25 +51,38 @@ test_that("create_html_table okay", {
   st1 <- fx(create_html_table(df1))
   res1 <- "<div>\n <table>\n  <tr><th>f1</th><th>f2</th></tr>\n  <tr><td>1</td><td>A</td></tr>\n  <tr><td>2</td><td>B</td></tr>\n </table>\n</div>"
   expect_equal(st1,res1)
-  st2 <- fx(create_html_table(df1,class="aap-noot"))
+  st2 <- fx(create_html_table(df1,tableclass="aap-noot"))
   res2 <- "<div>\n <table class=\"aap-noot\">\n  <tr><th>f1</th><th>f2</th></tr>\n  <tr><td>1</td><td>A</td></tr>\n  <tr><td>2</td><td>B</td></tr>\n </table>\n</div>"
   expect_equal(st2,res2)
-  st3 <- fx(create_html_table(df1,class="aap-noot",header = list(c("X","Y"))))
+  st3 <- fx(create_html_table(df1,tableclass="aap-noot",header = list(c("X","Y"))))
   res3 <- "<div>\n <table class=\"aap-noot\">\n  <tr><th>X</th><th>Y</th></tr>\n  <tr><td>1</td><td>A</td></tr>\n  <tr><td>2</td><td>B</td></tr>\n </table>\n</div>"
   expect_equal(st3,res3)
-  st4 <- fx(create_html_table(df1,class="aap-noot",header = list(c("X1","Y1"),c("X2","Y2"))))
+  st4 <- fx(create_html_table(df1,tableclass="aap-noot",header = list(c("X1","Y1"),c("X2","Y2"))))
   res4 <- "<div>\n <table class=\"aap-noot\">\n  <tr><th>X1</th><th>Y1</th></tr>\n  <tr><th>X2</th><th>Y2</th></tr>\n  <tr><td>1</td><td>A</td></tr>\n  <tr><td>2</td><td>B</td></tr>\n </table>\n</div>"
   expect_equal(st4,res4)
-  st5 <- fx(create_html_table(df1,class="aap-noot", tstyle="border-collapse:collapse;border:none;"))
+  st5 <- fx(create_html_table(df1,tableclass="aap-noot", tablestyle="border-collapse:collapse;border:none;"))
   res5 <- "<div>\n <table class=\"aap-noot\" style=\"border-collapse:collapse;border:none;\">\n  <tr><th>f1</th><th>f2</th></tr>\n  <tr><td>1</td><td>A</td></tr>\n  <tr><td>2</td><td>B</td></tr>\n </table>\n</div>"
   expect_equal(st5,res5)
   st6 <- fx(create_html_table(df1, header=NULL))
   res6 <- "<div>\n <tabl>\n  <tr><td>1</td><td>A</td></tr>\n  <tr><td>2</td><td>B</td></tr>\n </table>\n</div>"
   expect_equal(st5,res5)
-  st7 <- fx( create_html_table(df1,class="fit-tablex",html_include=fit_table_def))
+  st7 <- fx( create_html_table(df1,tableclass="fit-tablex",html_include=fit_table_def))
   expect_equal(st7,fit_table_res)
   st8 <- fx(create_html_table(df1,caption="my caption"))
   res8 <- "<div>\n <table>\n  <caption>my caption</caption>\n  <tr><th>f1</th><th>f2</th></tr>\n  <tr><td>1</td><td>A</td></tr>\n  <tr><td>2</td><td>B</td></tr>\n </table>\n</div>"
   expect_equal(st8,res8)
+  st9 <- fx(create_html_table(df1,caption="my caption",captionstyle="white-space: nowrap;caption-side: bottom;"))
+  res9 <- "<div>\n <table>\n  <caption style=\"white-space: nowrap;caption-side: bottom;\">my caption</caption>\n  <tr><th>f1</th><th>f2</th></tr>\n  <tr><td>1</td><td>A</td></tr>\n  <tr><td>2</td><td>B</td></tr>\n </table>\n</div>"
+  expect_equal(st9,res9)
+  st10 <- fx(create_html_table(df1,caption="my caption",captionclass="mycaptionclass"))
+  res10 <- "<div>\n <table>\n  <caption class=\"mycaptionclass\">my caption</caption>\n  <tr><th>f1</th><th>f2</th></tr>\n  <tr><td>1</td><td>A</td></tr>\n  <tr><td>2</td><td>B</td></tr>\n </table>\n</div>"
+  expect_equal(st10,res10)
+  st11 <- fx(create_html_table(df1,thclass='TH',tdclass="TD"))
+  res11 <- "<div>\n <table>\n  <tr><th class=\"TH\">f1</th><th class=\"TH\">f2</th></tr>\n  <tr><td class=\"TD\">1</td><td class=\"TD\">A</td></tr>\n  <tr><td class=\"TD\">2</td><td class=\"TD\">B</td></tr>\n </table>\n</div>"
+  expect_equal(st11,res11)
+  st12 <- fx(create_html_table(df1,tdstyle="white-space: nowrap;"))
+  res12 <- "<div>\n <table>\n  <tr><th>f1</th><th>f2</th></tr>\n  <tr><td style=\"white-space: nowrap;\">1</td><td style=\"white-space: nowrap;\">A</td></tr>\n  <tr><td style=\"white-space: nowrap;\">2</td><td style=\"white-space: nowrap;\">B</td></tr>\n </table>\n</div>"
+  expect_equal(st12,res12)
+
 
 })
